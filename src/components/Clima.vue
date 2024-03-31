47d5a5644b37108c505728c544066668f8384693
Christian Blades <christianblades2332@gmail.com>
2024-03-31 17:09:43 -0500

    first commit

---
+++ src/components/Clima.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+    import useClima from '../composables/useClima';
+
+    const { formaterTemperatura } = useClima();
+    defineProps({
+        clima: {
+            type: Object,
+            default: true
+        },
+    })
+
+</script>
+
+<template>
+
+    <div class="resultado">
+        <h2>Clima de: {{ clima.name }}</h2>
+        <p class="actual">{{ formaterTemperatura (clima.main.temp) }}&deg;C</p>
+        
+        <div class="temperaturas">
+            <p>Min: <span>{{ formaterTemperatura (clima.main.temp_min) }}&deg;C</span></p>
+            <p>Min: <span>{{ formaterTemperatura (clima.main.temp_max) }}&deg;C</span></p>
+        </div>
+    </div>
+
+</template>
